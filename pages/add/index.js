a411892a4f1ac9df5c7de1de9bf3f6a54eb7a025
张森 <475306196@qq.com>
2017-12-15 23:39:22 +0800

    zbs: 增加了add页

--- pages/add/index.js
+++ pages/add/index.js
@@ -1,4 +1,19 @@
 
 Page({
-  
+  data: {
+    src: ""
+  },
+  chooseImg: function(){
+    var that = this;
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'], 
+      success: function (res) {
+        that.setData({
+          src: res.tempFilePaths
+        })
+      }
+    })
+  }
 })
