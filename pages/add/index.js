e1ae0b767d7eb3c51b2b88e365f332ae0ce8e06d
张森 <475306196@qq.com>
2018-01-04 21:47:46 +0800

    新建develop分支保存群主加机器人为好友

--- pages/add/index.js
+++ pages/add/index.js
@@ -5,14 +5,18 @@ Page({
     shareTicket: "",
     openGId:"",
     groupName:"",
-    groupDesc:""
+    groupDesc:"",
+    //模态窗旗帜
+    showModalStatus: false,
+    wxName: ""
+
   },
   onLoad: function (option) {
     var that = this
     wx.getShareInfo({
       shareTicket: option.shareTicket,
       success: function(res) {
-        console.log(res)
+        // console.log(res)
         wx.request({
           url: 'https://group.mrourou.com/wx/decrypt/data',
           header: {
@@ -25,7 +29,7 @@ Page({
             iv: res.iv
           },
           success: res => {
-            console.log(res)
+            // console.log(res)
             that.setData({
               openGId:res.data.data.openGId
             })
@@ -48,7 +52,14 @@ Page({
     })
   },
   formSubmit: function (e) {
-    console.log('form发生了submit事件，携带数据为：', e)
+    console.log('form发生了submit事件，携带数据为：', e);
+    this.setData({
+      groupName: e.detail.value.input,
+      groupDesc: e.detail.value.textarea
+    });
+
+    var that = this;
+
     wx.request({
       url: 'https://group.mrourou.com/wx/group',
       header: {
@@ -57,13 +68,58 @@ Page({
       },
       method: 'PUT',
       data: {
-        groupName: groupNa,
-        groupImage: groupImg,
-        groupDesc:groupDesc
+        groupImage: that.data.src,
+        groupName: that.data.groupName,
+        groupDesc: that.data.groupDesc,
+        openGId: that.data.openGId
       },
       success: res => {
-        console.log(res)
+        //判断群主跟机器人是否为好友
+        if(!res.isFriend){
+          //让群主拉机器人进群
+          wx.showModal({
+            title: '提示',
+            content: '输入您的微信号，加好友',
+            success: function (res) {
+              if (res.confirm) {
+                console.log('用户点击确定')
+                //显示模态窗
+                that.setData({
+                  showModalStatus: true
+                })
+
+              } else if (res.cancel) {
+                console.log('用户点击取消')
+              }
+            }
+          })
+
+        }
       }
     })
   },
+
+  //点击模态窗消失
+  disappear: function(){
+    this.setData({
+      showModalStatus: false
+    })
+  },
+
+  //点击模态窗的确定
+  tapOk: function(){
+
+    // 关闭模态窗
+    this.setData({
+      showModalStatus: false
+    })
+    
+  },
+
+  //模态窗确定按钮
+  perSubmit: function(e) {
+    console.log(e);
+  }
+
+
 })
